c4bdfa36cd4bc206abb9d3664bf784daa3d91cb2
wanxwang <wanxin-wang@uiowa.edu>
2018-11-03 14:05:12 -0500

    logger integeration is done

--- OpenGL_Demo/GLTracer/libGLTracer.h
+++ OpenGL_Demo/GLTracer/libGLTracer.h
@@ -1,22 +1,97 @@
 #ifndef _LIBGLTRACER_H
 #define _LIBGLTRACER_H
-#ifndef TRACEGL
-#define TRACEGL_1(func, p1)\
-	printf("name=%s, file=%s, line=%d\n", #func, __FILE__, __LINE__);\
-	func(p1)
+
+#define GL_FUNC_LEN			64
+#define GL_PATH_LEN			1024
+typedef struct LogVoidItem_t
+{
+	char func[GL_FUNC_LEN];
+	unsigned int tmDur; 			//on windows: in milliseconds, on linux: in microseconds
+	char filePath[GL_PATH_LEN];
+	unsigned int nLine;
+} LogVoidItem;
+
+#ifdef WIN32
+	#ifdef GLTRACER_EXPORTS
+		#define _GLTRACER_API __declspec(dllexport)
+	#else
+		#define _GLTRACER_API __declspec(dllimport)
+	#endif
+#else
+	#define _GLTRACER_API
 #endif
-#define TRACEGL_VOID_1(func, p1)\
-	printf("S:name=%s, file=%s, line=%d\n", #func, __FILE__, __LINE__);\
-	func(p1);\
-	printf("E\n");
 
-#define TRACEGL_VOID_3(func, p1, p2, p3)\
-	printf("S:name=%s, file=%s, line=%d\n", #func, __FILE__, __LINE__);\
-	func(p1, p2, p3);\
-	printf("E\n");
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	_GLTRACER_API LogVoidItem* FuncLogVoidStart(const char* funName, const char* fileName, unsigned int nLine);
+	_GLTRACER_API void FuncLogVoidEnd(LogVoidItem* item);
+#ifdef __cplusplus
+}
+#endif
 
+#ifndef GLTRACE_VOID_0
+#define GLTRACE_VOID_0(func)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func();\
+		FuncLogVoidEnd(item);\
+	}
+#endif
 
+#ifndef GLTRACE_VOID_1
+#define GLTRACE_VOID_1(func, p1)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func(p1);\
+		FuncLogVoidEnd(item);\
+	}
+#endif
 
+#ifndef GLTRACE_VOID_2
+#define GLTRACE_VOID_2(func, p1, p2)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func(p1, p2);\
+		FuncLogVoidEnd(item);\
+	}
+#endif
 
+#ifndef GLTRACE_VOID_3
+#define GLTRACE_VOID_3(func, p1, p2, p3)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func(p1, p2, p3);\
+		FuncLogVoidEnd(item);\
+	}
+#endif
+
+#ifndef GLTRACE_VOID_4
+#define GLTRACE_VOID_4(func, p1, p2, p3, p4)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func(p1, p2, p3, p4);\
+		FuncLogVoidEnd(item);\
+	}
+#endif
+
+#ifndef GLTRACE_VOID_5
+#define GLTRACE_VOID_5(func, p1, p2, p3, p4, p5)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func(p1, p2, p3, p4, p5);\
+		FuncLogVoidEnd(item);\
+	}
+#endif
+
+#ifndef GLTRACE_VOID_6
+#define GLTRACE_VOID_6(func, p1, p2, p3, p4, p5, p6)\
+	{\
+		LogVoidItem* item = FuncLogVoidStart(#func, __FILE__, __LINE__);\
+		func(p1, p2, p3, p4, p5, p6);\
+		FuncLogVoidEnd(item);\
+	}
+#endif
 
 #endif
